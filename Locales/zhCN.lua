c844edbaea5c0d1d091c11a9d7ccb9cf6ecc3517
HanFeng <trendhanfeng@gmail.com>
2020-06-15 22:23:30 +0800

    rename to WoWChatBot

--- Locales/zhCN.lua
+++ Locales/zhCN.lua
@@ -1,5 +1,4 @@
-local L = LibStub("AceLocale-3.0"):NewLocale("HF_AutoReply", "zhCN")
+local L = LibStub("AceLocale-3.0"):NewLocale("WoWChatBot", "zhCN")
 
-L["plugin_name"] = "ChatBot - 聊天机器人"
-
+L["plugin_name"] = "WoWChatBot - 聊天机器人"
 L["autoInvite_enable_name"] = "开启自动邀请"
